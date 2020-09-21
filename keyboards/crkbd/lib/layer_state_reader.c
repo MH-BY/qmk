e49d699d1419f20fe48be0cef2f61c01237a2b9e
MH-BY <59770282+MH-BY@users.noreply.github.com>
2020-09-22 07:39:58 +0900

    Update layer_state_reader.c

--- keyboards/crkbd/lib/layer_state_reader.c
+++ keyboards/crkbd/lib/layer_state_reader.c
@@ -8,6 +8,8 @@
 #define L_LOWER 2
 #define L_RAISE 4
 #define L_ADJUST 8
+#define L_FUNCTION 16
+#define L_NUMBS 32
 #define L_ADJUST_TRI 14
 
 char layer_state_str[24];
@@ -16,15 +18,23 @@ const char *read_layer_state(void) {
   switch (layer_state)
   {
   case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: QWERTY");
     break;
   case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: UI");
     break;
   case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: SYMBOLS");
     break;
   case L_ADJUST:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: ADJUST");
+    break;
+  case L_FUNCTION:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: FUNCTION");
+    break;
+  case L_NUMBS:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: NUMBS");
+    break;
   case L_ADJUST_TRI:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
     break;
